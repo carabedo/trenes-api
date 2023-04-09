8dff8d67a0fa3613a7ceb55a56e0d411cb7eb2d6
fernando <64593873+carabedo@users.noreply.github.com>
2023-04-09 16:28:26 -0300

    api

---
+++ api.js
@@ -0,0 +1,72 @@
+import fetch from 'node-fetch';
+import config from './config.js';
+import generateCredentials from './auth.js';
+import cache from './cache.js';
+import logger from './logger.js';
+
+const UNAUTH = 'Unauthorized';
+
+class Api {
+  constructor() {
+    this.token = cache.load();
+  }
+
+  async generateToken() {
+    const { token } = await this.internalRequest({
+      method: 'POST',
+      body: generateCredentials(),
+      path: config.auth,
+    });
+    this.token = token;
+    cache.save(token);
+    logger.log(`Generated new token: ${token}`);
+  }
+
+  async request(params) {
+    try {
+      if (this.isTokenExpired()) {
+        throw new Error(UNAUTH);
+      }
+      return this.internalRequest(params);
+    } catch (err) {
+      if (err.message === UNAUTH) {
+        await this.generateToken();
+        return this.internalRequest(params);
+      }
+      throw err;
+    }
+  }
+
+  async internalRequest({ method = 'GET', body, path, query = {} }) {
+    const options = {
+      method,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    };
+    if (this.token) {
+      options.headers.Authorization = this.token;
+    }
+
+    const url = new URL(`${config.url}${path}`);
+    Object.keys(query).forEach(key => url.searchParams.append(key, query[key]));
+
+    const res = await fetch(url, options);
+    if (!res.ok) {
+      throw new Error(res.status === 401 ? UNAUTH : res.statusText);
+    }
+    const resJson = await res.json();
+    return resJson;
+  }
+
+  isTokenExpired() {
+    if (!this.token) {
+      return true;
+    }
+    const tokenData = JSON.parse(Buffer.from(this.token.split('.')[1], 'base64').toString());
+    const expirationDate = tokenData.exp * 1000;
+    const now = new Date().getTime();
+    return now > expirationDate;
+  }
+}
+
+export default Api;
---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import Api from './api.js';
+import logger from './logger.js';
+
+const app = express();
+const api = new Api();
+
+app.get('*', logger.req, (req, res) =>
+  api
+    .request(req)
+    .then(apiRes => res.send(apiRes))
+    .catch(err => {
+      logger.err(err);
+      res.status(500).send(err.message);
+    })
+);
+
+app.listen(3000);
